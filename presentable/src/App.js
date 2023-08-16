afb90d970444902cf8885cf71cad56ccaba8bb83
lorena842911 <lorena842911@gmail.com>
2023-08-15 22:19:56 -0500

    Primera parte del entregable

---
+++ presentable/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='App'>
+        <form></form>
+        <Card/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
